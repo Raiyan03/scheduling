1a2f86e795fb1df8a7d432fcd3eb350753430064
Ryan <ryansheikh003@gmail.com>
2024-05-15 03:42:05 -0600

    Api wasn't working after deployment on vercel making some changes with index file

--- scheduleapi/urls.py
+++ scheduleapi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('greet/', views.greet, name='greet'),
+    path('', views.index),
 ]
--- scheduleapi/views.py
+++ scheduleapi/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
+from datetime import datetime
+
+from django.http import HttpResponse
+
 
 def greet(request):
     name = request.GET.get('name', 'Guest')
     return JsonResponse({'response': name})
 
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
